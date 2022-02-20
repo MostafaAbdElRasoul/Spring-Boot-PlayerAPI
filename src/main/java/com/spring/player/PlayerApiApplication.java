5adca42d4113e6b1571eacc43a634b9cfb760b1d
mostafa <mosta.abdo250@gmail.com>
2022-02-20 19:55:16 +0200

    Connect to Database

--- src/main/java/com/spring/player/PlayerApiApplication.java
+++ src/main/java/com/spring/player/PlayerApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PlayerApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlayerApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlayerApiApplication.class, args);
+    }
 
 }
--- src/test/java/com/spring/player/PlayerApiApplicationTests.java
+++ src/test/java/com/spring/player/PlayerApiApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlayerApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
