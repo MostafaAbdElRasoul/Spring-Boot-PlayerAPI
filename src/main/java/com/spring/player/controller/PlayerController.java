cf20fa517db10603cd7536c98e73a7654a2262ff
mostafa <mosta.abdo250@gmail.com>
2022-02-22 20:41:51 +0200

    Get All Players

---
+++ src/main/java/com/spring/player/controller/PlayerController.java
@@ -0,0 +1,27 @@
+package com.spring.player.controller;
+
+import com.spring.player.model.Player;
+import com.spring.player.service.PlayerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class PlayerController {
+
+    private PlayerService playerService;
+
+    @Autowired
+    public PlayerController(PlayerService playerService) {
+        this.playerService = playerService;
+    }
+
+    @GetMapping("players")
+    public String getPlayers(Model model){
+        model.addAttribute("players", playerService.allPlayers());
+        return "home";
+    }
+}
